acd33a2c56d03b28dc2e86775a0eb41baf4a7e76
LinoRino <omurakenshin@gmal.com>
2023-06-07 11:20:56 +0900

    change language en->jp

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[show edit update]
+  before_action :set_article, only: %i[show edit update destroy]
 
   def index
     @articles = Article.all
@@ -13,11 +15,12 @@ class ArticlesController < ApplicationController
 
   def edit; end
 
+
   def create
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to article_url(@article), notice: 'Article was successfully created.'
+      redirect_to article_url(@article), notice: t('articles_controller.create.flash_success')
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,10 +28,15 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      redirect_to @article, notice: "Article was successfully updated."
+      redirect_to @article, notice: t('articles_controller.update.flash_success')
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_url, notice: t('articles_controller.destroy.flash_success')
   end
 
   private
